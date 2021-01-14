a25e8fc9f7e240bee612f186e4cc42d3ffae3b3d
martell411 <75603673+martell411@users.noreply.github.com>
2021-01-14 16:41:27 +0200

    Matrix algoritms 3

--- main.cpp
+++ main.cpp
@@ -1,73 +1,42 @@
+//обернути матрицю на 180 градусів
+
 #include <iostream>
-#include <fstream>
-
+#include <time.h>
 using namespace std;
-
 int main()
 {
-    int size = 0, maxSum= 0;
-    int** matrix = new int*[size];
+  srand(time(0));
     
-    ifstream file_to_read("/Users/marta/Documents/Matrix2/Matrix2/matrix2.txt");
-    
-    if(!file_to_read.is_open())
-    {
-        cout << "File couldn't be open";
-        return -1;
-    }
-     file_to_read >> size;
-        
-    for(int i = 0; i < size; i++)
-        matrix[i] = new int[size];
-    
-    for(int i =0; i < size; i++)
-    {
-        for(int j = 0; j < size; j++)
-        {
-            file_to_read >> matrix[i][j];
-        }
-    }
-    
-    for (int i = 0, k = 0, y = 0; k < size + size; i++, k++)
-      {
-          int currSum = 0;
-          if (k < size - 1)
-          {
-              for (int j = y; j >= 0; j--)
-              {
-                  currSum += matrix[i++][j];
-              }
-              y++;
-              i = -1;
-          }
-          else if (k >= size)
-          {
-              for (int j = size - 1, x = i; j >= i; j--)
-              {
-                  currSum += matrix[x++][j];
-              }
-          }
-          if (currSum > maxSum) { maxSum = currSum; }
-      }
-      for (int i = 0; i < size; i++)
-      {
-          for (int j = 0; j < size; j++)
-          {
-              cout << matrix[i][j] << "\t";
-          }
-          cout << endl;
-      }
-      cout << "Max sum: " << maxSum;
+  int n;
+  cout << "Enter n: ";
+  cin >> n;
+  cout << "Original matrix:" << endl;
+  int** matr = new int* [n];
+  for (int i = 0; i < n; i++)
+    matr[i] = new int[n];
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < n; j++)
+      matr[i][j] = rand() % 17;
 
-      for (int i = 0; i < size; i++)
-      {
-          delete[] matrix[i];
-      }
-      
-      delete[] matrix;
-      matrix = NULL;
-         
-         
-    
-        
+  for (int i = 0; i < n; i++)
+  {
+    cout << endl;
+    for (int j = 0; j < n; j++)
+      cout << matr[i][j] << " ";
+  }
+  cout << "180 degrees matrix:" << endl;
+  int** matr1 = new int* [n];
+  for (int i = 0; i < n; i++)
+    matr1[i] = new int[n];
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < n; j++)
+      matr1[i][j] = matr[n - 1 - i][n - 1 - j];
+  for (int i = 0; i < n; i++)
+  {
+    cout << endl;
+    for (int j = 0; j < n; j++)
+      cout << matr1[i][j] << " ";
+  }
+  return 0;
+  system("pause");
 }
