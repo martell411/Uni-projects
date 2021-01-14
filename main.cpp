d8979e2e2b0e6570510cd9c0a6304bcccfcca373
martell411 <75603673+martell411@users.noreply.github.com>
2021-01-14 16:48:58 +0200

    Exam task 2
    uses matrix and string

--- main.cpp
+++ main.cpp
@@ -3,93 +3,55 @@
 
 using namespace std;
 
-//Задано три цілі числа. Визначити серед них максимальне і добуток двох іншим чисел.
+
 void F1()
 {
-    int f =0, s =0, t= 0;
-    cout << "f:";
-    cin >> f;
-    cout << "s:";
-    cin >> s;
-    cout << "t:";
-    cin >> t;
-    if(f > s && f > t)
+    int n, max=0,min=10;
+    cout << "Enter number: " << endl;
+    cin >> n;
+    while (n > 0)
     {
-        cout << "max:" << f << "mult:" << s*t;
+      if (n % 10 > max)
+        max = n % 10;
+      else if (n % 10 < min)
+        min = n % 10;
+      n /= 10;
+      
     }
-    else if(s > f && s > t)
-    {
-        cout << "max:" << s << "mult:" << f*t;
-    }
-    else cout << "max:" << t << "mult:" << f*s;
-}
- //Дано послідовність слів, відокремлених комами. Надкрукувати слово, складене з останніх літер усіх слів.
-void F2()
-{
-    string s, new_string;
-    char c;
-    cin >> s;
-    for(int i = 0; i < s.length(); i++)
-    {
-        if(s[i] == ',')
-        {
-            c = s[i- 1];
-            new_string += c;
-        }
-    }
-    cout << new_string;
+    cout << "Max: " << max << endl;
+    cout << "Min: " << min << endl;
+    cout << "Max multiplied by min:" << max * min << endl;
 }
 
-
-int sum(int* s, int j, int size)
-{
-  int result = 0;
-  for (int i = j; i < size; i++)
-  {
-    result += s[i];
-  }
-  return result;
-}
-
-//11.3
 int main()
 {
-  srand(time(0));
-    
-  int n, m;
-  cout << "Enter n: ";
-  cin >> n;
-  cout << "Enter m: ";
-  cin >> m;
-  int** matr = new int* [n];
-  for (int i = 0; i < n; i++)
-    matr[i] = new int[m];
-  for (int i = 0; i < n; i++)
-    for (int j = 0; j < m; j++)
-      matr[i][j] = rand() % 11;
-  cout << "First matrix:" << endl;
-  for (int i = 0; i < n; i++)
-  {
-    cout << endl;
-    for (int j = 0; j < m; j++)
-      cout << matr[i][j] << " ";
-  }
-  int** matrb = new int* [n];
-  for (int i = 0; i < n; i++)
-    matrb[i] = new int[m];
-  cout << endl;
-  for (int i = 0; i < n; i++)
-    for (int j = 0; j < m; j++)
+    string str;
+    int sum=0, pos=0, space=0,len = 0;
+    cout << "Enter string" << endl;
+    getline(cin, str);
+    for(int i = 0; i < str.length(); i++)
     {
-      matrb[i][j] = sum(matr[i], j, m);
+        if(str[i] == ' ')
+        {
+            space++;
+            
+            if(space == 3 || str[i] == '\0')
+            len = i + 1;
+            
+        }
     }
-  cout << "Second matrix:" << endl;
-  for (int i = 0; i < n; i++)
-  {
-    cout << endl;
-    for (int j = 0; j < m; j++)
-      cout << matrb[i][j] << " ";
-  }
-
     
+    for(int i = pos; i < len; i++)
+    {
+        if(str[i] != ' ')
+          {
+              space++;
+              sum++;
+              pos = i+1;
+          }
+       
+     }
+    
+   
+    cout << sum;
 }
