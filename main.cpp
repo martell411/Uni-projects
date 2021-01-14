166323026513d3efe9c62ba1f96eec6aa371424a
martell411 <75603673+martell411@users.noreply.github.com>
2021-01-14 16:44:16 +0200

    Basic code
    uses bool var

--- main.cpp
+++ main.cpp
@@ -1,42 +1,28 @@
-//обернути матрицю на 180 градусів
-
 #include <iostream>
-#include <time.h>
+
 using namespace std;
+
 int main()
 {
-  srand(time(0));
-    
-  int n;
-  cout << "Enter n: ";
-  cin >> n;
-  cout << "Original matrix:" << endl;
-  int** matr = new int* [n];
-  for (int i = 0; i < n; i++)
-    matr[i] = new int[n];
-  for (int i = 0; i < n; i++)
-    for (int j = 0; j < n; j++)
-      matr[i][j] = rand() % 17;
-
-  for (int i = 0; i < n; i++)
-  {
-    cout << endl;
-    for (int j = 0; j < n; j++)
-      cout << matr[i][j] << " ";
-  }
-  cout << "180 degrees matrix:" << endl;
-  int** matr1 = new int* [n];
-  for (int i = 0; i < n; i++)
-    matr1[i] = new int[n];
-  for (int i = 0; i < n; i++)
-    for (int j = 0; j < n; j++)
-      matr1[i][j] = matr[n - 1 - i][n - 1 - j];
-  for (int i = 0; i < n; i++)
-  {
-    cout << endl;
-    for (int j = 0; j < n; j++)
-      cout << matr1[i][j] << " ";
-  }
-  return 0;
-  system("pause");
+    int count, num, q1 = -1, sum = 0;
+    bool flag = 0;
+    cout << "Enter amount of numbers:" << "\t";
+    cin >> count;
+    if(count <= 0)
+        cout << "Error !";
+    else{
+        for(int i = 0; i < count; i++)
+        {
+            cin >> num;
+            if(num == 0)
+                flag = 1;
+            
+            if(flag == 1)
+            {
+                q1++;
+                sum += num;
+            }
+        }
+        cout << "Result: " << sum / q1 << endl;
+    }
 }
