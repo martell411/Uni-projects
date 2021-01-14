a9983321f90254e2c02bfa1c3e5d7c4630f60439
martell411 <75603673+martell411@users.noreply.github.com>
2021-01-14 16:40:03 +0200

    Matrix algorithms 2
    uses files

--- main.cpp
+++ main.cpp
@@ -3,73 +3,71 @@
 
 using namespace std;
 
-void ShowMatrix(int** a, int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            cout << a[i][j] << "\t";
-        }
-        cout << endl;
-    }
-}
-
 int main()
 {
-    // Добуток елементів в тих рядках, які не містять відємних елементів
-    int size = 10;
-    int** mtxint = new int* [size];
-    int  mult = 1;
-    bool flag;
-    do {
-        cout << "Enter N:";
-        cin >> size;
-    } while (size <= 0);
-    for (int i = 0; i < size; i++)
-    {
-        mtxint[i] = new int[size];
-    }
-    cout << "Enter array: " << endl;
-    for (int i = 0; i < size; ++i)
-    {
-        for (int j = 0; j < size; ++j)
-        {
-            cin >> mtxint[i][j];
-        }
-        cout << endl;
-    }
-    for (int i = 0; i < size; i++)
-    {
-        flag = true;
-        for (int j = 0; j < size; j++)
-        {
-            if (mtxint[i][j] > 0)
-            {
-                mult *= mtxint[i][j];
-            }
-            else
-            {
-                flag = false;
-            }
-
-        }
-
-        if (flag != false)
-            cout << "Mult: " << mult << endl;
-    }
-
-    ShowMatrix(mtxint, size);
-
-
-    for (int i = 0; i < size; i++)
-    {
-        delete[] mtxint[i];
-    }
-    delete[] mtxint;
-    mtxint = NULL;
+    int size = 0, maxSum= 0;
+    int** matrix = new int*[size];
     
+    ifstream file_to_read("/Users/marta/Documents/Matrix2/Matrix2/matrix2.txt");
+    
+    if(!file_to_read.is_open())
+    {
+        cout << "File couldn't be open";
+        return -1;
+    }
+     file_to_read >> size;
+        
+    for(int i = 0; i < size; i++)
+        matrix[i] = new int[size];
+    
+    for(int i =0; i < size; i++)
+    {
+        for(int j = 0; j < size; j++)
+        {
+            file_to_read >> matrix[i][j];
+        }
+    }
+    
+    for (int i = 0, k = 0, y = 0; k < size + size; i++, k++)
+      {
+          int currSum = 0;
+          if (k < size - 1)
+          {
+              for (int j = y; j >= 0; j--)
+              {
+                  currSum += matrix[i++][j];
+              }
+              y++;
+              i = -1;
+          }
+          else if (k >= size)
+          {
+              for (int j = size - 1, x = i; j >= i; j--)
+              {
+                  currSum += matrix[x++][j];
+              }
+          }
+          if (currSum > maxSum) { maxSum = currSum; }
+      }
+      for (int i = 0; i < size; i++)
+      {
+          for (int j = 0; j < size; j++)
+          {
+              cout << matrix[i][j] << "\t";
+          }
+          cout << endl;
+      }
+      cout << "Max sum: " << maxSum;
 
+      for (int i = 0; i < size; i++)
+      {
+          delete[] matrix[i];
+      }
+      
+      delete[] matrix;
+      matrix = NULL;
+         
+         
+    
+        
 }
-
-
