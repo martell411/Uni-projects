11dffc6c941ec4920266df561bca95fe12573b88
martell411 <75603673+martell411@users.noreply.github.com>
2021-01-14 16:45:10 +0200

    Basic algorithms using string

--- main.cpp
+++ main.cpp
@@ -1,28 +1,66 @@
 #include <iostream>
+#include <string.h>
 
 using namespace std;
 
+//Дано послідовність слів, відокремлених пропусками, в кінці крапка. Видалити останню букву з кожного слова.
+void DeleteLastLitter()
+{
+    string str;
+    getline(cin, str);
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (str[i] == ' ')
+        {
+            str.erase(i - 1, 1);
+        }
+
+    }
+
+    cout << str;
+}
+
+
+
+
+//Виводить символи а- а також їх коди в десятковому і шістнадцятковому вигляді
+void SymbolsAndTheirCode()
+{
+    char s;
+    int n;
+    for(s = 'a', n = 97; s <= 'z'&& n <= 122; s++, n++)
+    {
+        cout << "Symbol: " << s << "\t"
+        << "10: " << n << endl;
+        
+    }
+    
+    for(s = 'a', n = 97; s <= 'z'&& n <= 122; s++, n++)
+    {
+        cout << "16: " << (int)s << hex << "\t" << endl;
+    }
+}
+
+
+void SymetricWords()
+{
+    string str;
+    bool check = true;
+    getline(cin, str);
+    for (int i = 0; i < str.length()/2; i++)
+    {
+        if (str[i] != str[str.length()-i-1])
+        {
+            check = false;
+         }
+    }
+    if (check)
+       cout<< str << endl;
+    
+    
+}
 int main()
 {
-    int count, num, q1 = -1, sum = 0;
-    bool flag = 0;
-    cout << "Enter amount of numbers:" << "\t";
-    cin >> count;
-    if(count <= 0)
-        cout << "Error !";
-    else{
-        for(int i = 0; i < count; i++)
-        {
-            cin >> num;
-            if(num == 0)
-                flag = 1;
-            
-            if(flag == 1)
-            {
-                q1++;
-                sum += num;
-            }
-        }
-        cout << "Result: " << sum / q1 << endl;
-    }
+    //SymetricWords();
+    SymbolsAndTheirCode();
 }
